acbf03e15556e33b94d846ebcb9e7be054485a0b
Bianca Andrade <Bianca.Andrade@AB-inbev.com>
2023-07-07 19:29:35 -0300

    Configuração para prettier, eslint e babel

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb'],
+  overrides: [
+    {
+      env: {
+        node: true,
+        browser: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: {},
+};
