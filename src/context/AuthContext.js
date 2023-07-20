1fb98dde2e9357877bf8c0a444b3ff7ab319cbbd
Bianca Andrade <Bianca.Andrade@AB-inbev.com>
2023-07-20 02:17:50 -0300

    ajusta jwt localStorage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 
 export const AuthContext = React.createContext();
 
@@ -8,3 +8,24 @@ export const AuthProvider = ({ children }) => {
 
   return <AuthContext.Provider value={{ authData, setAuthData }}>{children}</AuthContext.Provider>;
 };
+ */
+
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [authData, setAuthData] = useState({ token: '', user: {} });
+
+  return (
+    <AuthContext.Provider value={{ authData, setAuthData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+export { AuthProvider, AuthContext, useAuthContext };
--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,17 @@ const api = axios.create({
   baseURL: 'http://localhost:3000', // Substitua pela URL da sua API
 });
 
+
+// Adicione um interceptor de requisição para enviar o token em todas as requisições
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
 // Função para configurar o token JWT nas requisições
 export const setAuthToken = (token) => {
   if (token) {
