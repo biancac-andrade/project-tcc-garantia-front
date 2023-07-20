3ee222765e9518e7645c80f8dcc79a61fb930c99
Bianca Andrade <Bianca.Andrade@AB-inbev.com>
2023-07-20 02:18:13 -0300

    retira e fez uma copia

--- src/components/Usuário/Autenticação/LoginPage.jsx
+++ src/components/Usuário/Autenticação/LoginPage.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@mui/material';
 import api, { setAuthToken } from '../../../services/api';
-import { AuthContext } from '../../../context/AuthContext';
+import { useAuthContext } from '../../../context/AuthContext';
 import { useNavigate, Link } from 'react-router-dom';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { setAuthData } = useContext(AuthContext);
+  const { setAuthData } = useAuthContext();
   const navigate = useNavigate(); // Obtenha o objeto history
 
   const handleEmailChange = (event) => {
@@ -18,83 +18,22 @@ const LoginPage = () => {
     setPassword(event.target.value);
   };
 
-  /*   const handleLogin = () => {
-    api.post('/auth/login', { email, password })
-      .then((response) => {
-        const token = response.data.token;
-        setAuthToken(token); // Configura o token JWT nas requisições
-        setAuthData(token); // Armazena o token no estado global (por exemplo, usando o Context API)
-      // Navegar para a página de boas-vindas após o login
-      // eslint-disable-next-line no-restricted-globals
-      navigate('/welcome');
-      console.log('passou')
-      })
-      .catch((error) => {
-        console.error('Erro de autenticação:', error);
-        // Trate erros de autenticação
-      console.log('não passou')
-
-      });
-  };
- */
-
-  /*  const handleLogin = () => {
-    api.post('/auth/login', { email, password })
-      .then((response) => {
-        const token = response.data.token;
-        setAuthToken(token); // Configura o token JWT nas requisições
-  
-        // Fazer uma solicitação para obter os detalhes do usuário autenticado
-        api.get('/user/profile')
-          .then((response) => {
-            console.log('Detalhes do usuário:', response.data); // Adicione este console.log para verificar a resposta da API
-            const user = response.data.user;
-            const role = user.role;
-            
-            // Agora você pode diferenciar o tipo de usuário com base no campo "role"
-            if (role === 'admin') {
-              // Lógica para o usuário administrador
-
-               // Navegar para a página de boas-vindas após o login
-            // eslint-disable-next-line no-restricted-globals
-              navigate('/welcomeAdmin');
-              
-            } else if (role === 'funcionario') {
-              // Lógica para o usuário funcionário
-
-               // Navegar para a página de boas-vindas após o login
-            // eslint-disable-next-line no-restricted-globals
-            navigate('/welcomeFunc');
-            }
-  
-            // Armazene as informações do usuário no estado global ou local, conforme necessário
-            setAuthData({ token, user });
-  
-           
-          })
-          .catch((error) => {
-            console.error('Erro ao obter detalhes do usuário:', error);
-            // Trate erros ao obter detalhes do usuário
-          });
-      })
-      .catch((error) => {
-        console.error('Erro de autenticação:', error);
-        // Trate erros de autenticação
-      });
-  }; */
-
   const handleLogin = () => {
     api
       .post('/auth/login', { email, password })
       .then((response) => {
         const token = response.data.token;
+
+          // Armazene o token no localStorage
+        localStorage.setItem('token', token);
+
+        localStorage.setItem('userRole', response.data.role);
+        
         setAuthToken(token); // Configura o token JWT nas requisições
 
-        // Fazer uma solicitação para obter os detalhes do usuário autenticado
         api
           .get('/user/profile')
           .then((response) => {
-            console.log('Detalhes do usuário:', response.data);
             const user = response.data;
             const role = user.role;
 
