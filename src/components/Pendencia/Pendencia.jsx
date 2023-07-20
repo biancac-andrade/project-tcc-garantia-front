f5e3d37dcf423069136991b2d1aeda00eca6b7b1
Bianca Andrade <Bianca.Andrade@AB-inbev.com>
2023-07-19 22:24:09 -0300

    adiciona mudanca de status na pendencia

--- src/components/Pendencia/Pendencia.jsx
+++ src/components/Pendencia/Pendencia.jsx
@@ -4,10 +4,11 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 
+
 export const Pendencia = () => {
   const { id } = useParams();
 
-  const [replacements, setReplacements] = useState([]);
+  const [pendings, setPendings] = useState([]);
   const [statuses, setStatuses] = useState([]);
 
 
@@ -21,15 +22,15 @@ export const Pendencia = () => {
   };
 
   useEffect(() => {
-    fetchReplacements();
+    fetchPendings();
     fetchStatuses();
   }, [id]);
 
-  const fetchReplacements = async () => {
+  const fetchPendings = async () => {
     try {
       const response = await axios.get('http://localhost:3000/pending');
 
-      setReplacements(response.data);
+      setPendings(response.data);
 
       console.log('sera solicita', response.data);
     } catch (error) {
@@ -37,49 +38,50 @@ export const Pendencia = () => {
     }
   };
 
-
-  const handleCancelReplacement = async () => {
+  const handleCancelPending = async (pendingId) => {
     try {
-
       const pendingStatus = statuses.find((status) => status.status_type === 'solicitacao cancelada');
-
+  
       if (!pendingStatus) {
-        console.error('Status "pendente" não encontrado');
+        console.error('Status "solicitacao cancelada" não encontrado');
         return;
       }
-
-      const pendingData = {
-        replace_date: new Date(),
-        status: pendingStatus._id,
-        request: id,
-      };
   
-      await axios.put('http://localhost:3000/pending', pendingData);
-      console.log(status);
-
+      const pendingData = {
+        status: pendingStatus._id,
+      };
+    
+      await axios.put(`http://localhost:3000/pending/status/${pendingId}`, pendingData);
+      console.log('Status do pedido pendente alterado para "solicitacao cancelada"');
+  
+      // Após alterar o status, recarregue a lista de pendentes para mostrar a mudança na interface
+      fetchPendings();
     } catch (error) {
-      console.error('Erro ao enviar a substituição:', error);
+      console.error('Erro ao alterar o status do pendente:', error);
     }
   };
 
-  const handleCompleteReplacement = async () => {
+
+  const handleCompletePending = async (pendingId) => {
 
     try {
 
-      const pendingStatus = statuses.find((status) => status.status_type === 'solicitada');
+      const requestedStatus = statuses.find((status) => status.status_type === 'solicitada');
 
-      if (!pendingStatus) {
-        console.error('Status "pendente" não encontrado');
-        return;
-      }
+    if (!requestedStatus) {
+      console.error('Status "solicitada" não encontrado');
+      return;
+    }
+      
+    const replacementData = {
+      replace_date: new Date(),
+      status: requestedStatus._id,
+      pending: pendingId, 
+    };
 
-      const pendingData = {
-        pending_date: new Date(),
-        status: pendingStatus._id,
-        request: id,
-      };
-  
-      await axios.post('http://localhost:3000/replacement', pendingData);
+      await axios.post(`http://localhost:3000/replacement`, replacementData);
+      
+      console.log(replacementData);
 
     } catch (error) {
       console.error('Erro ao enviar a substituição:', error);
@@ -89,15 +91,15 @@ export const Pendencia = () => {
   return (
     <div>
       <h1>Substituições</h1>
-      {replacements.map((replacement) => (
-        <div key={replacement._id}>
-          <h2>Data de Substituição: {new Date(replacement.pending_date).toLocaleString()}</h2>
-          {Array.isArray(replacement.status) && replacement.status.map((status) => (
+      {pendings.map((pending) => (
+        <div key={pending._id}>
+          <h2>Data de Substituição: {new Date(pending.pending_date).toLocaleString()}</h2>
+          {Array.isArray(pending.status) && pending.status.map((status) => (
   <h3 key={status._id}>Status: {status.status_type}</h3>
 ))}
           
           <ul>
-            {replacement.request.map((request) => (
+            {pending.request.map((request) => (
               <li key={request._id}>
                 <h4>Data da Solicitação: {new Date(request.request_date).toLocaleString()}</h4>
                 <h4>Quantidade: {request.quantity}</h4>
@@ -118,10 +120,10 @@ export const Pendencia = () => {
             ))}
           </ul>
           <div>
-            <button onClick={() => handleCompleteReplacement(replacement._id)}>
+            <button onClick={() => handleCompletePending(pending._id)}>
               <CheckCircle />
             </button>
-            <button onClick={() => handleCancelReplacement(replacement._id)}>
+            <button onClick={() => handleCancelPending(pending._id)}>
               <Delete />
             </button>
           </div>
