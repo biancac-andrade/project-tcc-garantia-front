f25b657cb343d5716ac53e14c175edee2477f6c1
Bianca Andrade <Bianca.Andrade@AB-inbev.com>
2023-07-20 23:25:40 -0300

    ajusta para receber dados de /pendentes

--- src/components/Reposição/Reposicao.jsx
+++ src/components/Reposição/Reposicao.jsx
@@ -1,13 +1,27 @@
 import { CheckCircle, Delete } from '@mui/icons-material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const Reposicao = () => {
+
+  const { id } = useParams();
   const [replacements, setReplacements] = useState([]);
+  const [statuses, setStatuses] = useState([]);
+
+  const fetchStatuses = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/status');
+      setStatuses(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar os status:', error);
+    }
+  };
 
   useEffect(() => {
     fetchReplacements();
-  }, []);
+    fetchStatuses();
+  }, [id]);
 
   const fetchReplacements = async () => {
     try {
@@ -17,71 +31,96 @@ export const Reposicao = () => {
 
       console.log('sera solicita', response.data);
     } catch (error) {
-      console.error('Erro ao buscar as substituições:', error);
+      console.error('Erro ao buscar as reposições:', error);
     }
   };
 
-  const updateReplacementStatus = async (replacementId) => {
+  const handleCancelReplace= async (replaceId) => {
     try {
-      await axios.put(`http://localhost:3000/replacement/${replacementId}`, {
-        status: 'concluído'
-      });
+      const replacementStatus = statuses.find((status) => status.status_type === 'reposicao cancelada');
   
-      // Atualize a lista de substituições após a conclusão bem-sucedida
+      if (!replacementStatus) {
+        console.error('Status "solicitacao cancelada" não encontrado');
+        return;
+      }
+  
+      const replaceData = {
+        status: replacementStatus._id,
+      };
+    
+      await axios.put(`http://localhost:3000/replacement/status/${replaceId}`, replaceData);
+      console.log('Status do pedido pendente alterado para "solicitacao cancelada"');
+  
+      
       fetchReplacements();
     } catch (error) {
-      console.error('Erro ao atualizar o status da substituição:', error);
+      console.error('Erro ao alterar o status do pendente:', error);
     }
   };
 
-  const handleDeleteReplacement = (replacementId) => {
-    // Implemente a lógica para excluir a substituição com o ID fornecido
-    console.log('Excluir substituição com ID:', replacementId);
-  };
+  const handleCompleteReplace = async (replaceId) => {
 
-  const handleCompleteReplacement = (replacementId) => {
-    // Implemente a lógica para marcar a substituição como concluída com o ID fornecido
-    updateReplacementStatus(replacementId);
-
-    console.log('Marcar substituição como concluída com ID:', replacementId);
+    try {
+      const replacementStatus = statuses.find((status) => status.status_type === 'concluido');
+  
+      if (!replacementStatus) {
+        console.error('Status "solicitacao cancelada" não encontrado');
+        return;
+      }
+  
+      const replaceData = {
+        status: replacementStatus._id,
+      };
+    
+      await axios.put(`http://localhost:3000/replacement/status/${replaceId}`, replaceData);
+      console.log('Status do pedido pendente alterado para "solicitacao cancelada"');
+  
+      
+      fetchReplacements();
+    } catch (error) {
+      console.error('Erro ao alterar o status do pendente:', error);
+    }
   };
 
   return (
     <div>
       <h1>Substituições</h1>
-      {replacements.map((replacement) => (
-        <div key={replacement._id}>
-          <h2>Data de Substituição: {new Date(replacement.replace_date).toLocaleString()}</h2>
-          {Array.isArray(replacement.status) && replacement.status.map((status) => (
-  <h3 key={status._id}>Status: {status.status_type}</h3>
-))}
-          
+      {replacements.map((replace) => (
+        <div key={replace._id}>
+          <h2>Data de Substituição: {new Date(replace.replace_date).toLocaleString()}</h2>
+          {Array.isArray(replace.status) && replace.status.map((status) => (
+            <h3 key={status._id}>Status: {status.status_type}</h3>
+          ))}
           <ul>
-            {replacement.request.map((request) => (
-              <li key={request._id}>
-                <h4>Data da Solicitação: {new Date(request.request_date).toLocaleString()}</h4>
-                <h4>Quantidade: {request.quantity}</h4>
-                <ul>
-                  {request.product.map((product) => (
-                    <li key={product._id}>
+            {replace.pending.map((pending) => (
+              <li key={pending._id}>
+                <h4>Data da Solicitação: {new Date(pending.pending_date).toLocaleString()}</h4>
+                <h4>Quantidade: {pending.request[0].quantity}</h4>
+                {pending.request[0].product.map((product) => (
+                  <ul key={product._id}>
+                    <li>
                       <h5>Nome do Produto: {product.product_name}</h5>
                       <p>Descrição: {product.description}</p>
                       <p>Quantidade: {product.quantity}</p>
                       <p>
                         Imagem: <img src={product.image} alt="Imagem do produto" />
                       </p>
-                      <p>Tipo: {product.type}</p>
+                      <ul>
+                        {product.type.map((type) => (
+                          <li key={type._id}>Tipo: {type.name_type}</li>
+                        ))}
+                      </ul>
                     </li>
-                  ))}
-                </ul>
+                  </ul>
+                ))}
               </li>
             ))}
           </ul>
           <div>
-            <button onClick={() => handleCompleteReplacement(replacement._id)}>
+            <button onClick={() => handleCompleteReplace(replace._id)}>
               <CheckCircle />
             </button>
-            <button onClick={() => handleDeleteReplacement(replacement._id)}>
+            <button onClick={() => handleCancelReplace(replace._id)}>
               <Delete />
             </button>
           </div>
